37feb02f0fd1ca0b71e2b44ada4714b2ac2540ca
HideMiura <hugomira1111@gmail.com>
2017-12-13 21:54:25 +0700

    migration cake_cms

---
+++ myapp/config/Migrations/20171213143523_Initial.php
@@ -0,0 +1,53 @@
+<?php
+use Migrations\AbstractMigration;
+
+class Initial extends AbstractMigration
+{
+
+    public $autoId = false;
+
+    public function up()
+    {
+
+        $this->table('users')
+            ->addColumn('id', 'integer', [
+                'autoIncrement' => true,
+                'default' => null,
+                'limit' => 10,
+                'null' => false,
+                'signed' => false,
+            ])
+            ->addPrimaryKey(['id'])
+            ->addColumn('username', 'string', [
+                'default' => null,
+                'limit' => 50,
+                'null' => true,
+            ])
+            ->addColumn('password', 'string', [
+                'default' => null,
+                'limit' => 255,
+                'null' => true,
+            ])
+            ->addColumn('role', 'string', [
+                'default' => null,
+                'limit' => 20,
+                'null' => true,
+            ])
+            ->addColumn('created', 'datetime', [
+                'default' => null,
+                'limit' => null,
+                'null' => true,
+            ])
+            ->addColumn('modified', 'datetime', [
+                'default' => null,
+                'limit' => null,
+                'null' => true,
+            ])
+            ->create();
+    }
+
+    public function down()
+    {
+        $this->dropTable('users');
+    }
+}
