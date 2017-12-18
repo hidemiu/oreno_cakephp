f9402c370c2bd035bf05d892f1f8e86ba621b867
HideMiura <hugomira1111@gmail.com>
2017-12-18 07:40:30 +0700

    modify

--- myapp/src/Controller/ArticlesController.php
+++ myapp/src/Controller/ArticlesController.php
@@ -3,12 +3,78 @@
 
 namespace App\Controller;
 
+use App\Controller\AppController;
+use Cake\Utility\Text;
+
 class ArticlesController extends AppController
 {
+    public function initialize()
+    {
+        parent::initialize();
+
+        $this->loadComponent('Paginator');
+        $this->loadComponent('Flash'); // FlashComponent をインクルード
+    }
+
     public function index()
     {
         $this->loadComponent('Paginator');
         $articles = $this->Paginator->paginate($this->Articles->find());
         $this->set(compact('articles'));
     }
+
+    public function view($slug = null)
+    {
+        $article = $this->Articles->findBySlug($slug)->firstOrFail();
+        $this->set(compact('article'));
+    }
+
+    public function add()
+    {
+        $article = $this->Articles->newEntity();
+        if ($this->request->is('post')) {
+            $article = $this->Articles->patchEntity($article, $this->request->getData());
+
+            // user_id の決め打ちは一時的なもので、あとで認証を構築する際に削除されます。
+            $article->user_id = 1;
+
+            if ($this->Articles->save($article)) {
+                $this->Flash->success(__('Your article has been saved.'));
+                return $this->redirect(['action' => 'index']);
+            }
+            $this->Flash->error(__('Unable to add your article.'));
+        }
+        $this->set('article', $article);
+    }
+
+    public function beforeSave($event, $entity, $options)
+    {
+        if ($entity->isNew() && !$entity->slug) {
+            $sluggedTitle = Text::slug($entity->title);
+            // スラグをスキーマで定義されている最大長に調整
+            $entity->slug = substr($sluggedTitle, 0, 191);
+        }
+
+        // これは一時的なもので、後で認証を構築するときに
+        // 削除されます。
+        if (!$entity->user_id) {
+            $entity->user_id = 1;
+        }
+    }
+
+    public function edit($slug)
+    {
+        $article = $this->Articles->findBySlug($slug)->firstOrFail();
+        if ($this->request->is(['post', 'put'])) {
+            $this->Articles->patchEntity($article, $this->request->getData());
+            if ($this->Articles->save($article)) {
+                $this->Flash->success(__('Your article has been updated.'));
+                return $this->redirect(['action' => 'index']);
+            }
+            $this->Flash->error(__('Unable to update your article.'));
+        }
+
+        $this->set('article', $article);
+    }
+
 }
--- myapp/src/Template/Articles/index.ctp
+++ myapp/src/Template/Articles/index.ctp
@@ -1,10 +1,12 @@
 <!-- File: src/Template/Articles/index.ctp -->
 
 <h1>記事一覧</h1>
+<?= $this->Html->link('記事の追加', ['action' => 'add']) ?>
 <table>
     <tr>
         <th>タイトル</th>
         <th>作成日時</th>
+        <th>操作</th>
     </tr>
 
     <!-- ここで、$articles クエリーオブジェクトを繰り返して、記事の情報を出力します -->
@@ -16,6 +18,9 @@
             </td>
             <td>
                 <?= $article->created->format(DATE_RFC850) ?>
+            </td>
+            <td>
+                <?= $this->Html->link('編集', ['action' => 'edit', $article->slug]) ?>
             </td>
         </tr>
     <?php endforeach; ?>
